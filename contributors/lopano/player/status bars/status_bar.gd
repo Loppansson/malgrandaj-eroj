50b1c7eddbe5dc1c21841de35b00e95072e16c70
Lopano <loppan@e.email>
2023-04-25 14:46:09 +0200

    Started creating StatusBar.

---
+++ contributors/lopano/player/status bars/status_bar.gd
@@ -0,0 +1,11 @@
+class_name StatusBar
+extends ProgressBar
+
+#=---------------------------------------------------=#
+#                     Status Bar                      #
+#=---------------------------------------------------=#
+# Dependencies: StatusSystem.                         #
+#=---------------------------------------------------=#
+# Displayes a status from a StatusSystem node. This   #
+# node must be a parent or specified by a path.       #
+#=---------------------------------------------------=#
