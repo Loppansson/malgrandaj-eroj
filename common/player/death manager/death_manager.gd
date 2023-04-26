3d5013567dfbbfcf8e822211976b64f407ff86c8
Lopano <loppan@e.email>
2023-04-26 16:43:36 +0200

    Moved death manager to world.

---
+++ common/player/death manager/death_manager.gd
@@ -0,0 +1,32 @@
+extends Node
+
+
+@export var status_system : StatusSystem
+@onready var scene_manager = get_tree().get_root().get_node(
+		"Main/SceneManager"
+)
+@onready var player = $".."
+@onready var _player_start_pos = player.get("global_position")
+
+func _ready():
+	assert(
+			status_system.has_signal("status_at_min"),
+			"The given Node is not of type StatusSystem."
+	)
+	
+	status_system.connect(
+			"status_at_min",
+			Callable(
+					self,
+					"_on_status_system_status_at_min"
+			)
+	)
+
+
+func _on_status_system_status_at_min(status_name : String):
+	if status_name == "health":
+		status_system.set_status_current(
+				status_name,
+				status_system.get_status_default(status_name)
+		)
+		player.set("global_position", _player_start_pos)
--- contributors/lopano/debug tools/db_give_damage.gd
+++ contributors/lopano/debug tools/db_give_damage.gd
@@ -6,8 +6,3 @@ extends Node
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		status_system.add_status_current("health", -1)
-
-
-func _on_status_system_status_at_min(status):
-	if status == "health":
-		print("You Died!!")
